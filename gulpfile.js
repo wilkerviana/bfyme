0feb8337cbf6dbe97f982731b88edf7fcba115a0
wilkerviana <wilker.info@gmail.com>
2017-09-14 23:12:14 -0300

    Add configuração de tasks [style, view e browserSync].

--- gulpfile.js
+++ gulpfile.js
@@ -1,9 +1,47 @@
-'use strict';
-
+"use strict";
 const gulp  = require('gulp'),
       bs    = require('browser-sync'),
       view  = require('gulp-pug'),
       plumb = require('gulp-plumber'),
-      style = require('gulp-scss'),
+      style = require('gulp-sass'),
       image = require('gulp-imagemin'),
       watch = require('gulp-watch');
+
+gulp.task('bs', function(){
+  bs.init({
+    server:{
+      baseDir: 'dist'
+    }
+  });
+
+  gulp.watch('src/**/*.js', ['script']);
+  gulp.watch('src/**/*.scss', ['style']);
+  gulp.watch('src/**/*.pug', ['view']);
+  gulp.watch('dist/**/*').on('change', () => {
+    bs.reload();
+  });
+});
+
+gulp.task('style',() => {
+  gulp.src('src/styles/*.scss')
+      .pipe(plumb({
+        handleError: function(error){
+          console.log(error);
+          this.emit('end');
+        }
+      }))
+      .pipe(style())
+      .pipe(gulp.dest('dist/css/'));
+});
+
+gulp.task('view',() => {
+  gulp.src('src/view/*.pug')
+      .pipe(view({
+        pretty: true
+      }))
+      .pipe(gulp.dest('dist/'));
+});
+
+
+// default task with BrowserSync
+gulp.task('default',['bs']);
