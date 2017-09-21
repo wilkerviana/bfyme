61233b4b654aed4b51239b236b6c9cfed0a64612
Paulo Reis <paulojrn82@gmail.com>
2017-09-21 08:03:10 -0300

    estilos

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -1,6 +1,5 @@
 window.addEventListener('load',() => {
   const menuIcon = document.querySelector('.icon-hamburguer');
-  
   menuIcon.addEventListener('click',() => {
     menuIcon.classList.toggle('active');
     document.querySelector('.menu').classList.toggle('active');
