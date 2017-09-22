a7a1e434b14e897a85855c7e8bb2958bbb5d69bd
wilkerviana <wilker.info@gmail.com>
2017-09-22 01:03:53 -0300

    Add estilo e configuração de slider responsivo [wireframe] (media queries incompleto)

--- src/scripts/animate.js
+++ src/scripts/animate.js
@@ -9,9 +9,28 @@ window.addEventListener('load',() => {
 
 $(document).ready(function(){
   $('.slider').slick({
-    infinite: false,
+    infinite: true,
     slidesToShow: 4,
-    slidesToScroll: 1
+    slidesToScroll: 1,
+    prevArrow:"<button type='button' class='slick-prev'><img src='images/arrow_slider-prev.png' /></button>",
+    nextArrow:"<button type='button' class='slick-next'><img src='images/arrow_slider-next.png' /></button>",
+    responsive: [
+      {
+        breakpoint: 800,
+        settings: {
+          slidesToShow: 2,
+          slidesToScroll: 2
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+    ]
+
   });
 });
 
